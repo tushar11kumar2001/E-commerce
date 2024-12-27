c9a7771a4bbfbcbf4f3252a7fc030c7062da1a07
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-12-27 22:17:27 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(cors());
+
+app.use("/",(req, res)=>{
+    res.send("hello fuck")
+});
+
+
+module.exports = app;
